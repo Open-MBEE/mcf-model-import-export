e595ebb002b18b5239d03a7234d43ab49d9a403a
McDaniel, Donte S <donte.s.mcdaniel@lmco.com>
2020-01-29 16:41:42 -0500

    Added updates to plugi, getting orgs and projects, added UI portion

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ const express = require('express');
 const app = express();
 // Require the authentication module
 const {authenticate} = M.require('lib.auth');
+const organizationController = M.require('controllers.organization-controller');
+const projectController = M.require('controllers.project-controller'); 
 
+// Use the public folder as our static folder from which we can serve static files
+app.use(express.static(__dirname+'/public'));
 // Defining the main route
-app.get('/', authenticate, (req, res) => {
-    res.send('Export Plugin Route Successful');
+app.get('/', authenticate, (req, res, next) => {
+    res.sendFile('index.html');
+});
+
+// Defining the getAllData route for initial page loads
+app.get('/getAllData', authenticate, async (req, res) => {
+    // Getting organizations and projects of the user
+    let results = { organizations: await organizationController.find(req.user), projects: {}};
+    for (let i =0; i < results.organizations.length; i++) {
+        let projects = await projectController.find(req.user, results.organizations[i]._id);
+        results.projects[results.organizations[i]._id] = projects;
+    }
+    // Sending results to the client
+    res.send(results);
 });
 
 // Export/expose the app
