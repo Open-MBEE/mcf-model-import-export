3d20508062b8987f84f240ba90be0d145136012c
McDaniel, Donte S <donte.s.mcdaniel@lmco.com>
2020-02-05 15:20:25 -0500

    Finished up import

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const projectController = M.require('controllers.project-controller');
 const branchController = M.require('controllers.branch-controller');
 const elementController = M.require('controllers.element-controller');
 const utils = M.require('lib.utils');
+const getPublicData = M.require('lib.get-public-data');
+const log = M.require('lib.logger');
+
+// Creating a logger
+const logger = log.makeLogger('start');
 
 // Initializing Globals
 let orgs;
@@ -51,16 +56,16 @@ app.post('/branches', authenticate, (req, res) => {
 // POST route for export
 app.post('/export', authenticate, async (req, res) => {
     let body = JSON.parse(req.body);
-    let model = await elementController.find(req.user, body.organization, body.project, body.branch);
-    let updatedModel = removeReferences(model);
+    let elements = await elementController.find(req.user, body.organization, body.project, body.branch);
+    let updatedModel = removeReferences(elements);
     res.send(updatedModel);
 });
 
 // POST route for import
 app.post('/import', authenticate, async (req, res) => {
     let body = JSON.parse(req.body);
-    console.log(body);
-    // TODO: Import the model into the database
+    importData(req.user, body.data, body.organization, body.project, body.branch);
+    res.send({'status': 'complete'});
 });
 
 /**
@@ -96,109 +101,112 @@ async function getInitialData(user) {
 }
 
 /**
- * Removes all references of organizations, projects, branches, etc.
- * @param {Array} model array of elements for a given model
+ * Imports the data into the database
+ * @param {Array} data array of element objects
+ * @param {String} organization name of the organization
+ * @param {String} project name of the project
+ * @param {String} branch name of the branch
  */
-function removeReferences(model) {
-    // Looping through each element in the model
-    for (let i = 0; i < model.length; i++) {
-        model[i]._id = model[i]._id.split(':')[model[i]._id.split(':').length - 1]; // Removing the org, project, branch
-        // parent property
-        if (model[i].parent) {
-            model[i].parent = model[i].parent.split(':')[model[i].parent.split(':').length - 1];
+async function importData(user, data, organization, project, branch) {
+    // Looping through each element in the imported data array
+    for (let i = 0; i < data.length; i++) {
+
+        // Delete all imported properties that cannot be changed
+        delete data[i].branch;
+        delete data[i].org;
+        delete data[i].project;
+        delete data[i].createdOn;
+        delete data[i].createdBy;
+        delete data[i].updatedOn;
+        delete data[i].lastModifiedBy;
+        delete data[i].contains;
+        delete data[i].sourceOf;
+        delete data[i].targetOf;
+
+        if (data[i].id === 'model' || data[i].id === '__mbee__' || data[i].id === 'undefined' || data[i].id === 'holding_bin') {
+            let nameSpacedElementID = `${organization}:${project}:${branch}:${data[i].id}`;
+            let element = await elementController.search(user, organization, project, branch, nameSpacedElementID);
+
+            if (data[i].id === 'holding_bin') {
+                element = await elementController.search(user, organization, project, branch, 'holding bin');
+            }
+
+            let changed = isEquivalent(data[i], element[0]);
+
+            if (data[i].id === 'model') {
+                // Cannot change parent
+                delete data[i].parent;
+            }
+            
+            if (changed.hasChanged) {
+                logger.info('Updating '+nameSpacedElementID);
+                elementController.update(user, organization, project, branch, data[i]);
+            }
         }
-        // source property
-        if (model[i].source) {
-            model[i].source = model[i].source.split(':')[model[i].source.split(':').length - 1];
+        else {
+            // Checking to see if the element exists
+            // If not, create a new element
+            let found = await elementController.find(user, organization, project, branch, data[i].id);
+            if (found.length !== 0) {
+                let nameSpacedElementID = `${organization}:${project}:${branch}:${data[i].id}`;
+                logger.info('Updating '+nameSpacedElementID);
+                elementController.update(user, organization, project, branch, data[i]);
+            }
+            else {
+                // Creating new element
+                logger.info('Creating new element');
+                elementController.create(user, organization, project, branch, data[i]);
+            }
         }
-        // target property
-        if (model[i].target) {
-            model[i].target = model[i].target.split(':')[model[i].target.split(':').length - 1];
-        }
-        // targetOf property
-        if (model[i].targetOf) {
-            model[i].targetOf.forEach(target => {
-                target._id = target._id.split(':')[target._id.split(':').length - 1];
-                // parent property
-                if (target.parent) {
-                    target.parent = target.parent.split(':')[target.parent.split(':').length - 1];
-                }
-                // source property
-                if (target.source) {
-                    target.source = target.source.split(':')[target.source.split(':').length - 1];
-                }
-                // target property
-                if (target.target) {
-                    target.target = target.target.split(':')[target.target.split(':').length - 1];
-                }
-                delete target.project;
-                delete target.branch;
-                delete target.createdBy;
-                delete target.lastModifiedBy;
-                delete target.updatedOn;
-                delete target.archivedOn;
-                delete target.archived;
-            });
-        }
-        // contains property
-        if (model[i].contains) {
-            model[i].contains.forEach(c => {
-                c._id = c._id.split(':')[c._id.split(':').length - 1];
-                // parent property
-                if (c.parent) {
-                    c.parent = c.parent.split(':')[c.parent.split(':').length - 1];
-                }
-                // source property
-                if (c.source) {
-                    c.source = c.source.split(':')[c.source.split(':').length - 1];
-                }
-                // target property
-                if (c.target) {
-                    c.target = c.target.split(':')[c.target.split(':').length - 1];
-                }
-                delete c.project;
-                delete c.branch;
-                delete c.createdBy;
-                delete c.lastModifiedBy;
-                delete c.updatedOn;
-                delete c.archivedOn;
-                delete c.archived;
-            });
-        }
-        // sourceOf property
-        if (model[i].sourceOf) {
-            model[i].sourceOf.forEach(source => {
-                source._id = source._id.split(':')[source._id.split(':').length - 1];
-                // parent property
-                if (source.parent) {
-                    source.parent = source.parent.split(':')[source.parent.split(':').length - 1];
-                }
-                // source property
-                if (source.source) {
-                    source.source = source.source.split(':')[source.source.split(':').length - 1];
-                }
-                // target property
-                if (source.target) {
-                    source.target = source.target.split(':')[source.target.split(':').length - 1];
-                }
-                delete source.project;
-                delete source.branch;
-                delete source.createdBy;
-                delete source.lastModifiedBy;
-                delete source.updatedOn;
-                delete source.archivedOn;
-                delete source.archived;
-            });
-        }
-        delete model[i].project;
-        delete model[i].branch;
-        delete model[i].createdBy;
-        delete model[i].lastModifiedBy;
-        delete model[i].updatedOn;
-        delete model[i].archivedOn;
-        delete model[i].archived;
     }
-    return model;
+
+    logger.info('Import Complete');
+    return true;
+}
+
+/**
+ * Removes all references of organizations, projects, branches, etc.
+ * by getting public data of the element
+ * @param {Array} elements array of elements
+ */
+function removeReferences(elements) {
+    let parsedElements = [];
+
+    // Looping through each element and getting the public data
+    for (let i = 0; i < elements.length; i++) {
+        let parsedElement = getPublicData.getPublicData(elements[i], 'element');
+        parsedElements.push(parsedElement);
+    }
+
+    logger.info('organization, project, and branch references REMOVED!');
+    return parsedElements;
+}
+
+/**
+ * Checks to see if object is equivalent
+ * @param {Object} a 
+ * @param {Object} b
+ * @returns {Object} 
+ * object.hasChanged is of type Boolean.
+ * object.propNames is of type Array. The name of the properties that have changed
+ */
+function isEquivalent(a, b) {
+    // Create arrays of property names
+    var aProps = Object.getOwnPropertyNames(a);
+    let changedProperties = [];
+    let hasChanged = false;
+
+    for (var i = 0; i < aProps.length; i++) {
+        var propName = aProps[i];
+
+        // If values of same property are not equal, objects are not equivalent
+        if (a[propName] !== b[propName]) {
+            changedProperties.push(propName);
+            hasChanged = true;
+        }
+    }
+
+    return {hasChanged, 'properties': changedProperties};
 }
 
 // Export/expose the app
